7c775e96137d60cb7c9e68b46d6a62313ecd2b38
ironmann350 <ironmann350@outlook.com>
2017-10-05 11:52:30 +0800

    Update cache_bust.py

--- tikeshell/templatetags/cache_bust.py
+++ tikeshell/templatetags/cache_bust.py
@@ -4,14 +4,16 @@ import random
 
 register = template.Library()                                                                                                            
 rand=random.WichmannHill()
+production=True
+#return nothing in production to enable caching 
+#for a faster load
 @register.simple_tag(name='cache_bust')                                                                                                  
 def cache_bust():                                                                                                                        
 
-    if settings.DEBUG:                                                                                                                   
-        version = random.randint(100,10000)                                                                                                          
+    if not production:                                                                                                                   
+        version = random.randint(100,10000)    
+        return '__v__={version}'.format(version=version)
     else:                                                                                                                                
-        version = os.environ.get('PROJECT_VERSION')                                                                                       
-        if version is None:                                                                                                              
-            version = '1'                                                                                                                
+        return ''                                                                                                               
 
-    return '__v__={version}'.format(version=version)
+    
